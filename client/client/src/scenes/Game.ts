25d81c9cdd8672e4f6f9f4ef724ef399d871f201
0xmugen <pierre.semanne@gmail.com>
2024-05-17 15:08:18 +0200

    fix fromating issue

--- client/client/src/scenes/Game.ts
+++ client/client/src/scenes/Game.ts
@@ -1,385 +1,263 @@
-import Phaser from "phaser";
-import IText from "phaser3-rex-plugins/plugins/gameobjects/dom/inputtext/InputText";
-import InputText from "phaser3-rex-plugins/plugins/inputtext.js";
-import config from "../config";
-import CRDT from "../networking/crdt";
-import Media from "../networking/media";
-import DefaultButton from "../ui-components/defaultButton";
-import { PeerInfo } from "./types";
+import { WebrtcProvider } from "y-webrtc";
+import * as Y from "yjs";
+import {
+  AudioContent,
+  InputContent,
+  PositionContent,
+  State,
+  TextContent,
+  UsernameContent,
+  resolutionMessage,
+  resolutionMessageLite,
+} from "../scenes/types";
+import { CRDT_CHAT_HISTORY_REMOTE, CRDT_PEER_STATE } from "./messages/crdt";
+import { YMap } from "yjs/dist/src/internals";
 
-interface connectionContainer {
-  yourId: IText;
-  otherId: IText;
-  connectButton: DefaultButton;
+export enum CRDT_STATE {
+  REMOVED = "REMOVED",
+  STATE = "STATE",
 }
 
-export default class Demo extends Phaser.Scene {
-  constructor() {
-    super("GameScene");
-  }
+export default class CRDT {
+  private doc: Y.Doc;
+  private chatHistoryRemote: Y.Array<CRDT_CHAT_HISTORY_REMOTE>;
+  private globalState: Y.Map<CRDT_PEER_STATE>;
+  private provider: WebrtcProvider;
 
-  private _connectButton: DefaultButton;
-  private prevConnectionBtn: DefaultButton;
-  private nextConnectionBtn: DefaultButton;
+  private isListening: Map<string, boolean>;
+  private peers: Map<number, Map<string, any>>;
+  private state: State;
 
-  private connectionIndex: number = 0;
+  private readonly AWARENESS = "AWARENESS";
 
-  private dialogue: InputText | undefined;
-  private username = "";
-  private inputText: InputText | undefined;
-  private lobbyInputText: InputText | undefined;
-
-  private connectionContainers: connectionContainer[] = [];
-
-  private removeSignalEvent: any = () => {};
-
-  //private scene : Phaser.Scene
-  preload() {}
-
-  create() {
-    this.renderAcceptButton();
-    this.renderWelcomeMessage();
-    this.renderInputText();
-    this.renderLobbyInputText();
-  }
-  renderPlatformerButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "Enter Platformer",
-        (config.scale.width / 4) * 2,
-        config.scale.height / 2 + 100,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          const lobbyName = this.lobbyInputText?.text || "";
-          const crdt = new CRDT(lobbyName);
-          const media = new Media(crdt.getClientID());
-
-          this.removeSignalEvent();
-          this.game.config.lobbyName = lobbyName;
-
-          this.scene.start("platformer", {
-            peers: this.game.config.connections.getConnections(),
-            username: this.username,
-            crdt: crdt,
-            media: media,
-          });
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  renderWelcomeMessage() {
-    var welcomeMessageConfig: IText.IConfig = {
-      text: "Welcome to esotere \n Select your username",
-      color: "black",
-      border: 1,
-      backgroundColor: "white",
-      readOnly: true,
-    };
-    var welcomeMessage = new InputText(
-      this,
-      (config.scale.width / 4) * 2,
-      config.scale.height / 2 - 200,
-      500,
-      50,
-      welcomeMessageConfig
-    );
-    this.add.existing(welcomeMessage);
-
-    this.dialogue = welcomeMessage;
-  }
-
-  renderInputText() {
-    var inputTextConfig: IText.IConfig = {
-      text: "",
-      color: "black",
-      border: 1,
-      backgroundColor: "white",
-    };
-    var inputText = new InputText(
-      this,
-      (config.scale.width / 4) * 2,
-      config.scale.height / 2 - 100,
-      500,
-      50,
-      inputTextConfig
-    );
-    this.add.existing(inputText);
-
-    // Set our input text as a member object
-    this.inputText = inputText;
-  }
-
-  renderLobbyInputText() {
-    var lobbyInputTextConfig: IText.IConfig = {
-      text: "",
-      color: "black",
-      border: 1,
-      backgroundColor: "white",
-      placeholder: "Enter Lobby Name",
-    };
-    var lobbyInputText = new InputText(
-      this,
-      (config.scale.width / 4) * 2,
-      config.scale.height / 2 - 50,
-      500,
-      50,
-      lobbyInputTextConfig
-    );
-    this.add.existing(lobbyInputText);
-
-    this.lobbyInputText = lobbyInputText;
-  }
-
-  renderAcceptButton() {
-    this._connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "Enter username",
-        (config.scale.width / 4) * 2,
-        config.scale.height / 2,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          let newUsername = this.inputText?.text || "";
-
-          if (newUsername.length > 0) {
-            this.username = newUsername;
-            this.dialogue?.setText(`Username : ${newUsername}`);
-            this._connectButton.destroy();
-            this.inputText?.destroy();
-            this.renderPlatformerButton();
-          }
-        }
-      )
-    );
-  }
-
-  renderConnectButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "Connect",
-        (config.scale.width / 4) * 2,
-        config.scale.height / 2 + 200,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          let index = this.connectionContainers.length;
-          this.connect(index - 1);
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  incrementConnectionIndex() {
-    this.connectionContainers[this.connectionIndex].yourId.setVisible(false);
-    this.connectionContainers[this.connectionIndex].otherId.setVisible(false);
-    this.connectionContainers[this.connectionIndex].connectButton.setVisible(
-      false
-    );
-    this.connectionIndex++;
-    if (this.connectionIndex >= this.connectionContainers.length - 1) {
-      this.nextConnectionBtn.disable();
-    }
-    this.prevConnectionBtn.enable();
-    this.connectionContainers[this.connectionIndex].yourId.setVisible(true);
-    this.connectionContainers[this.connectionIndex].otherId.setVisible(true);
-    this.connectionContainers[this.connectionIndex].connectButton.setVisible(
-      true
-    );
-  }
-
-  decrementConnectionIndex() {
-    this.connectionContainers[this.connectionIndex].yourId.setVisible(false);
-    this.connectionContainers[this.connectionIndex].otherId.setVisible(false);
-    this.connectionContainers[this.connectionIndex].connectButton.setVisible(
-      false
-    );
-    this.connectionIndex--;
-    if (this.connectionIndex <= 0) {
-      this.prevConnectionBtn.disable();
-    }
-    this.nextConnectionBtn.enable();
-    this.connectionContainers[this.connectionIndex].yourId.setVisible(true);
-    this.connectionContainers[this.connectionIndex].otherId.setVisible(true);
-    this.connectionContainers[this.connectionIndex].connectButton.setVisible(
-      true
-    );
-  }
-
-  renderConnectionScrollButtons() {
-    this.prevConnectionBtn = this.add.existing(
-      new DefaultButton(
-        this,
-        "Prev",
-        (config.scale.width / 6) * 2,
-        config.scale.height / 2 + 200,
-        config.scale.width / 16 - 10,
-        "large",
-        () => this.decrementConnectionIndex()
-      )
-    );
-
-    this.nextConnectionBtn = this.add.existing(
-      new DefaultButton(
-        this,
-        "Next",
-        (config.scale.width / 6) * 4,
-        config.scale.height / 2 + 200,
-        config.scale.width / 16 - 10,
-        "large",
-        () => this.incrementConnectionIndex()
-      )
-    );
-
-    this.prevConnectionBtn.disable();
-    this.nextConnectionBtn.disable();
-  }
-
-  renderInitializeConnectButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "+ Initialize Connection",
-        (config.scale.width / 4) * 2 - 150,
-        config.scale.height / 2 - 100,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          this.renderConnection(true);
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  renderRespondToConnectButton() {
-    var connectButton = this.add.existing(
-      new DefaultButton(
-        this,
-        "+ Respond to Connection",
-        (config.scale.width / 4) * 2 + 150,
-        config.scale.height / 2 - 100,
-        config.scale.width / 4 - 10,
-        "large",
-        () => {
-          this.renderConnection(false);
-        }
-      )
-    );
-
-    return connectButton;
-  }
-
-  renderConnection(is_initiator: boolean) {
-    var yourIdFieldConfig: IText.IConfig = {
-      text: "",
-      color: "black",
-      border: 1,
-      backgroundColor: "white",
-      readOnly: true,
-    };
-    var yourId = new InputText(
-      this,
-      (config.scale.width / 4) * 2,
-      config.scale.height / 2 + (is_initiator ? 0 : 100),
-      500,
-      50,
-      yourIdFieldConfig
-    );
-    this.add.existing(yourId);
-
-    var otherIdFieldConfig: IText.IConfig = {
-      text: "",
-      color: "black",
-      border: 1,
-      backgroundColor: "white",
-      readOnly: false,
-      placeholder: "paste your peers id here",
-    };
-    var otherId = new InputText(
-      this,
-      (config.scale.width / 4) * 2,
-      config.scale.height / 2 + (is_initiator ? 100 : 0),
-      500,
-      50,
-      otherIdFieldConfig
-    );
-
-    this.add.existing(otherId);
-
-    let connectButton = this.renderConnectButton();
-
-    if (
-      this.prevConnectionBtn == undefined ||
-      this.nextConnectionBtn == undefined
-    ) {
-      this.renderConnectionScrollButtons();
-    }
-
-    if (
-      this.connectionContainers.length == 1 ||
-      this.connectionContainers.length >= this.connectionIndex
-    ) {
-      this.nextConnectionBtn.enable();
-    }
-
-    if (this.connectionContainers.length > 0) {
-      yourId.setVisible(false);
-      otherId.setVisible(false);
-      connectButton.setVisible(false);
-    }
-
-    this.connectionContainers.push({
-      yourId,
-      otherId,
-      connectButton,
+  constructor(lobbyName: string) {    this.doc = new Y.Doc();
+    this.chatHistoryRemote = this.doc.getArray("chat-history");
+    this.provider = new WebrtcProvider(lobbyName, this.doc, {      
+      signaling: [`${process.env.SIGNALING_SERVER}`],
     });
 
-    this.initializePeer(is_initiator);
+    this.isListening = new Map();
+    this.peers = new Map();
+    this.state = {
+      username: undefined,
+      position: undefined,
+      input: undefined,
+      text: undefined,
+      audio: undefined,
+    };
+
+    this.globalState = this.doc.getMap("global-state");
+
+    console.log(`CRDT: Client ID is ${this.doc.clientID}.`);
   }
 
-  private attachSignalEvent({ peer, index }: PeerInfo) {
-    let username = this.username;
-
-    let connectionContainer = this.connectionContainers[index];
-
-    function signalData(data: any) {
-      //@ts-ignore
-      data.username = username;
-
-      if (connectionContainer.yourId.text.length == 0) {
-        connectionContainer?.yourId?.setText(JSON.stringify(data));
-      }
+  aware() {
+    if (this.isListening.get(this.AWARENESS) === true) {
+      return;
     }
 
-    this.removeSignalEvent = () => peer.removeListener("signal", signalData);
-    peer.on("signal", signalData);
+    this.provider.awareness.on("change", (changes: any) => {
+      console.log(`CRDT: Awareness change ${changes}.`);
+
+      const states = this.provider.awareness.getStates();
+
+      const setState = (clientID: number) => {
+        if (clientID === this.doc.clientID) {
+          return;
+        }
+
+        const state = states.get(clientID);
+
+        if (state === undefined) {
+          return;
+        }
+
+        if (this.peers.has(clientID) === false) {
+          this.peers.set(clientID, new Map());
+        }
+
+        this.peers.get(clientID)!.set(CRDT_STATE.STATE, state);
+      };
+
+      const setRemoved = (clientID: number) => {
+        if (clientID === this.doc.clientID) {
+          return;
+        }
+
+        if (this.peers.has(clientID) === false) {
+          return;
+        }
+
+        this.peers.get(clientID)!.set(CRDT_STATE.REMOVED, true);
+      };
+
+      changes.added.forEach(setState);
+
+      changes.updated.forEach(setState);
+
+      changes.removed.forEach(setRemoved);
+    });
+
+    this.isListening.set(this.AWARENESS, true);
   }
-  initializePeer(is_initiator: boolean) {
-    this.game.config.connections.initializePeer(
-      is_initiator,
-      true,
-      // bind the Game scene object to the `this` keyword so it is accessible in the callback function
-      this.attachSignalEvent.bind(this)
+
+  setUsername(username: UsernameContent) {
+    this.state.username = username;
+  }
+
+  setPosition(position: PositionContent) {
+    this.state.position = position;
+  }
+
+  setInput(input: InputContent) {
+    this.state.input = input;
+  }
+
+  setText(text: TextContent) {
+    this.state.text = text;
+
+    setTimeout(() => {
+      const nullifierText = {
+        text: "",
+        timestamp: 0,
+      };
+
+      this.state.text = nullifierText;
+      this.setChatHistoryRemote(nullifierText);
+    }, 5 * 1000);
+  }
+
+  setChatHistoryRemote(text: TextContent) {
+    this.chatHistoryRemote.push([
+      {
+        id: this.doc.clientID,
+        username: this.state.username ? this.state.username.username : "",
+        ...text,
+      },
+    ]);
+  }
+
+  setAudio(audio: AudioContent) {
+    this.state.audio = audio;
+  }
+
+  broadcastState() {
+    if (this.isListening.get(this.AWARENESS) !== true) {
+      return;
+    }
+
+    this.provider.awareness.setLocalState(this.state);
+  }
+
+  observeChatHistoryRemote(
+    callback: (chatHistoryRemote: Array<CRDT_CHAT_HISTORY_REMOTE>) => void
+  ) {
+    this.chatHistoryRemote.observe(
+      (event: Y.YArrayEvent<CRDT_CHAT_HISTORY_REMOTE>, _: Y.Transaction) => {
+        callback(event.target.toArray());
+      }
     );
   }
 
-  connect(index: number) {
-    console.log(this);
-    let selectedPeer = this.game.config.connections.getConnectionByIndex(index);
-    let connectionContainer = this.connectionContainers[index];
-
-    let username = JSON.parse(connectionContainer.otherId.text).username;
-    this.game.config.connections.updateUsername(index, username);
-    selectedPeer.peer.signal(connectionContainer.otherId.text);
+  getClientID(): number {
+    return this.doc.clientID;
   }
+
+  getChatHistoryRemote(): Array<CRDT_CHAT_HISTORY_REMOTE> {
+    return this.chatHistoryRemote.toArray();
+  }
+
+  getPeers(): Map<number, Map<string, any>> {
+    return this.peers;
+  }
+
+    //
+    // methods that access & manipulate CRDT global state
+    //
+    observeGlobalState(
+        callback: (globalState: Y.Map<CRDT_PEER_STATE>) => void
+    ) {
+        this.globalState.observe(
+            (event: Y.YMapEvent<CRDT_PEER_STATE>, tx: Y.Transaction) => {
+                // console.log(`globalState observe(): last event: ${JSON.stringify(event)}`);
+                callback(event.target);
+            }
+        );
+    }
+
+    addResolutionMessageToMyGlobalState (message: resolutionMessage) {
+        // grab my current message queue from crdt
+        const myClientID = this.doc.clientID;
+        let myCurrState = this.globalState.get(myClientID.toString());
+        if (myCurrState === undefined) {
+            myCurrState = {
+                username: undefined,
+                position: undefined,
+                input: undefined,
+                text: undefined,
+                audio: undefined,
+                messages: []
+            };
+        }
+        const myCurrMessageQueue = myCurrState.messages as resolutionMessage[];
+
+        // push new message to queue
+        const newMessageQueue = myCurrMessageQueue.concat([message]);
+
+        // update crdt
+        this.globalState.set(myClientID.toString(), {
+            ...myCurrState,
+            messages: newMessageQueue
+        });
+    }
+
+    clearMyMessageQueue () {
+        const myClientID = this.doc.clientID;
+
+        // check if my queue is already empty; if so, skip crdt update
+        const currQueue = this.globalState.get(myClientID.toString())?.messages;
+        if (currQueue === undefined) {
+            return;
+        }
+        if (currQueue.length == 0) {
+            return;
+        }
+
+        // otherwise clear the queue in crdt
+        const emptyState = {
+            username: undefined,
+            position: undefined,
+            input: undefined,
+            text: undefined,
+            audio: undefined,
+            messages: []
+        };
+        this.globalState.set(myClientID.toString(), emptyState);
+    }
+
+    addResolutionMessageToPeerMessageQueue (clientID: number, message: resolutionMessageLite) {
+        // grab the target message queue from crdt
+        let currState = this.globalState.get(clientID.toString());
+        if (currState === undefined) {
+            currState = {
+                username: undefined,
+                position: undefined,
+                input: undefined,
+                text: undefined,
+                audio: undefined,
+                messages: []
+            };
+        }
+        const currMessageQueue = currState.messages as resolutionMessageLite[];
+
+        // push new message to queue
+        const newMessageQueue = currMessageQueue.concat([message]);
+
+        // update crdt
+        this.globalState.set(clientID.toString(), {
+            ...currState,
+            messages: newMessageQueue
+        });
+    }
+
 }
